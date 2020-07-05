a8954726ef0b21e306c1d78fc9fc90c676b73858
Nagarajan CK <nagainvertibrate@gmail.com>
2020-07-05 16:33:28 +0530

    add endpoint for cup draw

--- src/main/java/com/fpl/minileague/controller/FPLController.java
+++ src/main/java/com/fpl/minileague/controller/FPLController.java
@@ -1,5 +1,7 @@
 package com.fpl.minileague.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 
 @RestController
 @RequestMapping(value = "/fpl")
@@ -36,7 +37,31 @@ public class FPLController {
 	@GetMapping(value = "/fixtures")
 	public Object GetMiniLeagueFixtures(@RequestParam String gameweek, @RequestParam String leagueID) {
 
-		return restTemplate.getForEntity(fplURL_fixtures+gameweek, Object.class,leagueID).getBody();
+		return restTemplate.getForEntity(fplURL_fixtures + gameweek, Object.class, leagueID).getBody();
 
 	}
+
+	@GetMapping(value = "/draw")
+	public String GetCupFixtures(@RequestParam int teams) {
+
+		String drawResult = "";
+		ArrayList<Integer> generated = new ArrayList<>();
+		while (generated.size() < teams) {
+			int num = (int) Math.floor(Math.random() * teams)+1;
+			if (!(generated.contains(num))) {
+				generated.add(num);
+			}
+		}
+
+		for (int index = 0; index < generated.size(); index++) {
+			drawResult += generated.get(index);
+			if (index % 2 == 0)
+				drawResult += "\tvs\t";
+			else
+				drawResult += "<br><br>";
+
+		}
+		return drawResult;
+	}
+
 }
